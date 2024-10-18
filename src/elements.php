6e407627c64945bcee74f4554cfe64cf79d50dee
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-19 00:07:10 +0100

    prueba casa

--- src/elements.php
+++ src/elements.php
@@ -2,7 +2,7 @@
 abstract class Element {
     protected $name;
     protected $basePrice;
-    protected static $tax = 0.07; 
+    protected static $tax = 0.07; // Prueba de rama en casa
 
     public function __construct($name, $basePrice) {
         $this->name = $name;
